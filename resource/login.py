2a222e1bb0b2e763921c1d34ed2b50ca823300c7
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-01 22:38:22 +0900

    adding some new resource for users

---
+++ resource/login.py
@@ -0,0 +1,6 @@
+from helper.resource import YuzukiResource
+
+
+class Login(YuzukiResource):
+    def __init__(self):
+        YuzukiResource.__init__(self)
--- route.py
+++ route.py
@@ -1,9 +1,13 @@
 from twisted.web.static import File
 
 from resource.hello import Hello
+from resource.login import Login
+from resource.register import Register
 
 ROUTE = {
     "": Hello(),
+    "login": Login(),
+    "register": Register(),
     "favicon.ico": File("static/img/favicon.ico"),
     "hello": Hello(),
 }
