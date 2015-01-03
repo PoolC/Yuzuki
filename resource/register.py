1267c6a74f2b6f0275f5444530c910390db71008
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-03 23:41:18 +0900

    register

--- resource/register.py
+++ resource/register.py
@@ -1,6 +1,15 @@
 from helper.resource import YuzukiResource
+from model.user import User
 
 
 class Register(YuzukiResource):
     def __init__(self):
         YuzukiResource.__init__(self)
+
+    def render_GET(self, request):
+        # TODO: show register page
+        pass
+
+    def render_POST(self, request):
+        # TODO: register user
+        pass
