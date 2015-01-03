3ce490c801371800c8afe81effdf76bd980b6a18
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-04 03:56:01 +0900

    group

---
+++ model/group.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from model.base import Base
+
+
+class Group(Base):
+    __tablename__ = "group"
+    uid = Column(Integer(), primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    description = Column(String(255))
+    users = relationship("User", secondary="assoc_user_group")
+
+    def __init__(self, name):
+        self.name = name
