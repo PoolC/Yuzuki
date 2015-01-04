9206203dc776eaa09f4ad1050ee7f584e27c8c15
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-05 01:40:03 +0900

    added route

--- route.py
+++ route.py
@@ -1,12 +1,17 @@
 from twisted.web.static import File
 
+from resource.index import Index
+from resource.api import Api
 from resource.hello import Hello
 from resource.login import Login
+from resource.logout import Logout
 from resource.register import Register
 
 ROUTE = {
-    "": Hello(),
+    "": Index(),
+    "api": Api(),
     "login": Login(),
+    "logout": Logout(),
     "register": Register(),
     "favicon.ico": File("static/img/favicon.ico"),
     "hello": Hello(),
