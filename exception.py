f4bcd33ea1acd888cef3cc7f39b8a9fed7df818d
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-07 05:37:18 +0900

    new exception bad argument

--- exception.py
+++ exception.py
@@ -6,14 +6,21 @@ class YuzukiException(Exception):
     """
     pass
 
+
 class MissingArgument(YuzukiException):
     status = 400
-    
+
     def __init__(self, key):
         YuzukiException.__init__(self, "Missing argument key is \"%s\"." % key)
 
+
 class DuplicateArgumentGiven(YuzukiException):
     status = 400
-    
+
     def __init__(self, key):
         YuzukiException.__init__(self, "Duplicate key \"%s\" is given as argument" % key)
+
+
+class BadArgument(YuzukiException):
+    def __init__(self, key, value):
+        YuzukiException.__init__(self, "Bad Argument \"%s=%s\"" % (key, value))
