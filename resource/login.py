873004b21864a2cfb432829fd0d39fca1edd2fa2
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-07 01:14:23 +0900

    handling for blocked use

--- resource/login.py
+++ resource/login.py
@@ -6,6 +6,9 @@ from model.user import User
 
 
 class Login(YuzukiResource):
+    INVALID_CREDENTIAL = u"ID나 비밀번호가 잘못되었습니다."
+    BLOCKED_USER = u"차단된 계정입니다."
+
     def __init__(self):
         YuzukiResource.__init__(self)
 
@@ -20,6 +23,8 @@ class Login(YuzukiResource):
         result = query.all()
         if result:
             user = result[0]
+            if user.is_blocked:
+                return self.render_template(request, self.BLOCKED_USER)
             if user.check_password(password):
                 request.log_user_in(user)
                 # TODO: remember me feature
@@ -28,16 +33,16 @@ class Login(YuzukiResource):
                 return "logged in"
             else:
                 request.setResponseCode(UNAUTHORIZED)
-                return self.render_login_page(request, True)
+                return self.render_login_page(request, self.INVALID_CREDENTIAL)
         else:
             request.setResponseCode(UNAUTHORIZED)
-            return self.render_login_page(request, True)
+            return self.render_login_page(request, self.INVALID_CREDENTIAL)
 
-    def render_login_page(self, request, error):
+    def render_login_page(self, request, err):
         redirect = request.get_argument("redirect", "")
         context = {
             "redirect": redirect
         }
-        if error:
-            context["err"] = u"ID나 비밀번호가 잘못되었습니다."
+        if err:
+            context["err"] = err
         return self.render_template("login.html", request, context)
