581ac24d116a9d7cc43f26141359f0656211eb8b
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-07 01:13:26 +0900

    group name need to be index. maybe. maybe not. who cares?

--- model/group.py
+++ model/group.py
@@ -8,7 +8,7 @@ from model.base import Base
 class Group(Base):
     __tablename__ = "group"
     uid = Column(Integer(), primary_key=True)
-    name = Column(String(255), nullable=False, unique=True)
+    name = Column(String(255), index=True, nullable=False, unique=True)
     description = Column(String(255))
     users = relationship("User", secondary="assoc_user_group")
     important = Column(Boolean, default=False)
