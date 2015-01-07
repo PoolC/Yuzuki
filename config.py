3424e9af90a85cbd859a740cf7bafa5c832b4a6d
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-07 23:17:01 +0900

    This is not DEBUG

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 
 ARTICLE_PER_PAGE = 15
 REPLY_PER_PAGE = 15
-DEBUG = True
+DEBUG = False
 SITE_NAME = "PoolC"
 DB_CONNECTION_STRING = 'sqlite:///sqlite.db'
 logger = logging.getLogger()
