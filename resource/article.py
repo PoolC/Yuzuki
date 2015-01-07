4da585dbb9caf6d1e358da648b8c4731d390f72a
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-07 15:59:04 +0900

    article delete

--- resource/article.py
+++ resource/article.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from twisted.web.http import NOT_FOUND
-
+from twisted.web.http import NOT_FOUND, UNAUTHORIZED
 from sqlalchemy.orm import subqueryload
 
 from helper.resource import YuzukiResource
@@ -33,11 +32,19 @@ class ArticleView(YuzukiResource):
                                                "Not Found",
                                                u"게시글이 존재하지 않습니다.")
         article = result[0]
-        context = {
-            "article": article,
-            "page": page,
-        }
-        return self.render_template("article_view.html", request, context)
+        if article.board.name == "notice" or (
+                    request.user and any([group.name == "anybody" for group in request.user.groups])):
+            context = {
+                "article": article,
+                "page": page,
+            }
+            return self.render_template("article_view.html", request, context)
+        else:
+            request.setResponseCode(UNAUTHORIZED)
+            return self.generate_error_message(request,
+                                               UNAUTHORIZED,
+                                               "Unauthorized",
+                                               u"게시글을 볼 권한이 없습니다.")
 
 
 class ArticleWrite(YuzukiResource):
@@ -45,4 +52,21 @@ class ArticleWrite(YuzukiResource):
 
 
 class ArticleDelete(YuzukiResource):
-    pass
+    def render_DELETE(self, request):
+        article_id = request.get_argument("id")
+        query = request.dbsession.query(Article). \
+            filter(Article.uid == article_id). \
+            filter(Article.enabled == True). \
+            options(subqueryload(Article.board))
+        result = query.all()
+        if not result:
+            request.setResponseCode(NOT_FOUND)
+            return "article not found"
+        article = result[0]
+        if request.user and (request.user == article.user or request.user.is_admin):
+            article.enabled = False
+            request.dbsession.commit()
+            return "delete success"
+        else:
+            request.setResponseCode(UNAUTHORIZED)
+            return "unauthorized user"
