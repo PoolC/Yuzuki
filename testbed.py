908fab30ab60eaf5de7ea00174cdffc4baf8a93a
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-08 20:07:18 +0900

    all models

--- testbed.py
+++ testbed.py
@@ -1,8 +1,15 @@
 from helper.database import DatabaseHelper
-from model.user import User
-from model.group import Group
 
 dbsession = DatabaseHelper.session()
+
+from model.user import User
+from model.group import Group
+from model.association.user_group import UserGroupAssociation
+from model.board import Board
+from model.article import Article
+from model.reply import Reply
+from model.article_record import ArticleRecord
+from model.reply_record import ReplyRecord
 
 import code
 code.interact(local=locals())
