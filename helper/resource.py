b5ae2a67fb8973ef60dbbbaaf7ee03959a7f911f
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-08 22:50:14 +0900

    changed logging format

--- helper/resource.py
+++ helper/resource.py
@@ -34,7 +34,10 @@ class YuzukiResource(Resource):
 
     def render(self, request):
         request.initialize(self)
-        request.logger.info(request.get_path_and_query() + " " + request.getClientIP())
+        log_str = request.method + " " + request.get_path_and_query()
+        if request.user:
+            log_str += " " + request.user.username
+        request.logger.info(log_str)
         # TODO: auto login
         result = Resource.render(self, request)
         request.finalize()
