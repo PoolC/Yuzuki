8f1010997c15cfa2943f6936783bb4a9a7bb4148
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-08 20:00:22 +0900

    table collation

--- model/base.py
+++ model/base.py
@@ -1,3 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+Base.__table_args__ = {
+    "mysql_charset": "utf8",
+    "mysql_collate": "utf8_general_ci",
+}
