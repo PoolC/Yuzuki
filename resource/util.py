7e3f395c42be79018b095668693d2040b9671162
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-09 20:36:35 +0900

    /foo/asdf now shows page not found

--- resource/util.py
+++ resource/util.py
@@ -6,6 +6,19 @@ from helper.template import generate_error_message
 
 
 class PageNotFound(YuzukiResource):
+    isLeaf = False
+
     def render_GET(self, request):
         request.setResponseCode(NOT_FOUND)
         return generate_error_message(request, NOT_FOUND, u"페이지를 찾을 수 없습니다")
+
+    def render_POST(self, request):
+        request.setResponseCode(NOT_FOUND)
+        return generate_error_message(request, NOT_FOUND, u"페이지를 찾을 수 없습니다")
+
+    def render_DELETE(self, request):
+        request.setResponseCode(NOT_FOUND)
+        return generate_error_message(request, NOT_FOUND, u"페이지를 찾을 수 없습니다")
+
+    def getChildWithDefault(self, path, request):
+        return self
