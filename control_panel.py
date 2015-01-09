f7f3a235fc624904d797aaee8e37c09b942ec664
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-09 16:52:00 +0900

    formatting and encoding

--- control_panel.py
+++ control_panel.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from helper.database import DatabaseHelper
 
 dbsession = DatabaseHelper.session()
--- helper/md_ext.py
+++ helper/md_ext.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
-from jinja2 import nodes, Markup
-from jinja2.ext import Extension
 from markdown import Markdown
 
 markdown_extension_list = [
-        "extra",
-        "admonition",
-        "codehilite(css_class=highlight)",
-        "nl2br",
-        "sane_lists",
-        "toc",
-        "wikilinks",
-    ]
+    "extra",
+    "admonition",
+    "codehilite(css_class=highlight)",
+    "nl2br",
+    "sane_lists",
+    "toc",
+    "wikilinks",
+]
 md = Markdown(extensions=markdown_extension_list)
+
 
 def markdown_convert(source):
     return md.convert(source)
