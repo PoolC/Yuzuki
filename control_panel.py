f6fc0303eb142f96f7f4f55301fe1c64845cce8a
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-11 04:20:57 +0900

    admin helper functions

--- control_panel.py
+++ control_panel.py
@@ -14,5 +14,20 @@ from model.user import User
 
 anybody = dbsession.query(Group).filter(Group.name == "anybody")[0]
 
+def get_user_by_username(username):
+    query = dbsession.query(User).filter(User.username == username)
+    result = query.all()
+    return result[0] if result else None
+
+def get_user_by_uid(uid):
+    query = dbsession.query(User).filter(User.uid == uid)
+    result = query.all()
+    return result[0] if result else None
+
+def get_users_not_anybody():
+    anybody_user_ids = [user.uid for user in anybody.users]
+    query = dbsession.query(User).filter(User.uid.notin_(anybody_user_ids))
+    return query.all()
+
 import code
 code.interact(local=locals())
