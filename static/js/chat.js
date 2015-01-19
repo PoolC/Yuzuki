f72e4b1cba1d9e920454fdff440e3a3d02f69bf3
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-19 14:16:38 +0900

    change id format because of duplicate id problem

--- static/js/chat.js
+++ static/js/chat.js
@@ -17,7 +17,7 @@ process_chat_items = function (data) {
     for (var i = 0; i < data.length; i++) {
         latest_uid = Math.max(latest_uid, data[i].uid);
         var chat_item = data[i];
-        var template = '<li class="chat-item row" id="{{uid}}"><div id="{{user_id}}" class="chat-item-user-nickname col-xs-2">{{user_nickname}}</div><div class="chat-content-datetime col-xs-10"><span class="chat-content">{{content}}</span><span class="chat-datetime">{{created_at}}</div></li>';
+        var template = '<li class="chat-item row" id="chat-{{uid}}"><div id="user-{{user_id}}" class="chat-item-user-nickname col-xs-2">{{user_nickname}}</div><div class="chat-content-datetime col-xs-10"><span class="chat-content">{{content}}</span><span class="chat-datetime">{{created_at}}</div></li>';
         var rendered = Mark.up(template, chat_item);
         message_ul.append(rendered);
         if (message_ul.children().length > chat_per_page) {
