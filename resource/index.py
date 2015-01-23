4354707fd8b274669cea4b265bcaf1b4232a4944
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-24 06:06:51 +0900

    localization concern

--- resource/index.py
+++ resource/index.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from helper.resource import YuzukiResource
 from helper.template import render_template
-from localization import SITE_DESCRIPTION
+from localization import SITE_DESCRIPTION, SITE_INDEX
 
 
 class Index(YuzukiResource):
     def render_GET(self, request):
         context = {"SITE_DESCRIPTION": SITE_DESCRIPTION}
-        return render_template("index.html", request, context)
+        return render_template(SITE_INDEX, request, context)
