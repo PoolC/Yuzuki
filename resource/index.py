2f37a7c53bb4c0cdfd55fb198fdc904250903f6c
Gyusun Yeom <omniavinco@gmail.com>
2015-10-08 19:35:51 +0900

    Serving index page with 'page/index' add index sample

--- resource/index.py
+++ resource/index.py
@@ -2,9 +2,17 @@
 from helper.resource import YuzukiResource
 from helper.template import render_template
 from localization import SITE_DESCRIPTION, SITE_INDEX
-
+from helper.content import md
+from StringIO import StringIO
 
 class Index(YuzukiResource):
     def render_GET(self, request):
-        context = {"SITE_DESCRIPTION": SITE_DESCRIPTION}
-        return render_template(SITE_INDEX, request, context)
+        content = StringIO()
+        with open("page/index") as f:
+            md.convertFile(f, content, "utf-8")
+            context = {
+                "SITE_DESCRIPTION": SITE_DESCRIPTION,
+                "content": content.getvalue().decode("utf-8"),
+            }
+            print context
+        return render_template("page.html", request, context)
