fde161aee69ec2c9300c57edd5a9930917e29428
Oh Jinkyun <tintypemolly@gmail.com>
2015-11-27 22:22:55 +0900

    slack noti fix

--- helper/slack.py
+++ helper/slack.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import json
-import StringIO
 import urllib
 
 from bleach import clean
 from twisted.internet import reactor
+from twisted.internet.ssl import ClientContextFactory
 from twisted.web.client import Agent, FileBodyProducer, readBody
 from twisted.web.http_headers import Headers
 
@@ -12,12 +12,17 @@ from config.config import SLACK_NOTI_CHANNEL, SLACK_POST_INFO, SLACK_NOTI_TARGET
 
 SLACK_CHAT_POST_MESSAGE_URL = "https://slack.com/api/chat.postMessage"
 
-agent = Agent(reactor)
+class WebClientContextFactory(ClientContextFactory):
+    def getContext(self, hostname, port):
+        return ClientContextFactory.getContext(self)
+
+contextFactory = WebClientContextFactory()
+agent = Agent(reactor, contextFactory)
 
 def post_message(request, article, article_view_url):
     if article.board.repr not in SLACK_NOTI_TARGET_BOARDS:
         return
-    article_link = request.getProto() + "://" + request.getHost() + article_view_url
+    article_link = request.getProto() + "://" + request.getRequestHostname() + article_view_url
     content = clean(article.compiled_content, tags=[], strip=True)
     if len(content) > 30:
         content = content[:27] + "..."
@@ -31,22 +36,23 @@ def post_message(request, article, article_view_url):
         "title": article.subject,
         "title_link": article_link,
         "text": content,
-        "author_name": article.author.nickname,
+        "author_name": article.user.nickname,
     })
     params["attachments"] = json.dumps(attachments)
-    body = FileBodyProducer(StringIO.StringIO(urllib.urlencode(params)))
+    params = {key: params[key].encode("utf-8") if type(params[key]) in (unicode, str) else params[key] for key in params}
+    get_args = urllib.urlencode(params)
 
     d = agent.request(
-        "POST",
-        "https://slack.com/api/chat.postMessage",
+        "GET",
+        "https://slack.com/api/chat.postMessage?" + get_args,
         Headers({"User-Agent": ["Twisted Web Server Slack Notify"]}),
-        body
+        None
     )
     d.addBoth(slack_callback, request)
 
 def slack_callback(resp, request):
-        d = readBody(resp)
-        d.addCallback(slack_resp, request)
+    d = readBody(resp)
+    d.addCallback(slack_resp, request)
 
 def slack_resp(resp_body, request):
     response = json.loads(resp_body)
