71f0991bd1165e5403348cf9d15b3ccb9f7b0a65
TintypeMolly <tintypemolly@gmail.com>
2016-03-18 00:03:35 +0900

    Update subscription.py
    table name convention

--- model/subscription.py
+++ model/subscription.py
@@ -2,7 +2,7 @@ from sqlalchemy import Table, Column, Integer, ForeignKey
 from model.base import Base
 
 subscription_table = Table(
-    "Subscription", Base.metadata,
+    "subscription", Base.metadata,
     Column("user", Integer, ForeignKey("user.uid")),
     Column("article", Integer, ForeignKey("article.uid"))
 )
